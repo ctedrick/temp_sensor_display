46e5545b9b9843d8586eb4425226652e169de4a1
Cody Tedrick <102986204+tedrick24@users.noreply.github.com>
2023-05-27 16:01:21 -0700

    init commit

---
+++ temp_sensor_display.ino
@@ -0,0 +1,52 @@
+
+
+#include <Arduino.h>
+#include <U8g2lib.h>
+#include <Wire.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+#pragma region 
+
+// Data wire is plugged into port 2 on the Arduino
+#define ONE_WIRE_BUS 2
+
+// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass our oneWire reference to Dallas Temperature. 
+DallasTemperature sensors(&oneWire);
+
+U8G2_SH1106_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+
+#pragma endregion
+
+void setup(void) {
+  sensors.begin();
+  u8g2.begin();
+}
+
+void loop(void) {
+  sensors.requestTemperatures(); // Send the command to get temperatures
+
+  float tempC = sensors.getTempFByIndex(0);
+
+  u8g2.setFont(u8g2_font_profont22_tf);
+  u8g2.firstPage();
+  do {
+    u8g2.setCursor(0, 20);
+    u8g2.setDrawColor(2);
+    u8g2.print(tempC);
+  } while ( u8g2.nextPage() );
+
+  delay(1000);
+}
+
+void lcdBold(bool aVal) {
+  if (aVal) {
+    u8g2.setFont(u8x8_font_saikyosansbold8_u);// BOLD
+  } else {
+    u8g2.setFont(u8g2_font_ncenB14_tr);// NORMAL
+  }
+}
+
