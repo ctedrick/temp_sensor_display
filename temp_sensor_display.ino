2a4f3d2c9e517d920483b0534e77d426fc131a6f
Cody Tedrick <cody.tedrick@gmail.com>
2023-05-27 16:25:20 -0700

    Add degree Fahrenheit symbol

--- temp_sensor_display.ino
+++ temp_sensor_display.ino
@@ -5,6 +5,7 @@
 #include <Wire.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <math.h>
 
 #pragma region 
 
@@ -21,6 +22,8 @@ U8G2_SH1106_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
 
 #pragma endregion
 
+const char DEGREE_SYMBOL[] = {"\xb0"};
+
 void setup(void) {
   sensors.begin();
   u8g2.begin();
@@ -29,14 +32,17 @@ void setup(void) {
 void loop(void) {
   sensors.requestTemperatures(); // Send the command to get temperatures
 
-  float tempC = sensors.getTempFByIndex(0);
+  float currTemp = sensors.getTempFByIndex(0);
 
-  u8g2.setFont(u8g2_font_profont22_tf);
+  u8g2.setFont(u8g2_font_ncenB14_tf);
   u8g2.firstPage();
   do {
     u8g2.setCursor(0, 20);
     u8g2.setDrawColor(2);
-    u8g2.print(tempC);
+
+    u8g2.print(currTemp);
+    u8g2.print(DEGREE_SYMBOL);
+    u8g2.print("F");
   } while ( u8g2.nextPage() );
 
   delay(1000);
