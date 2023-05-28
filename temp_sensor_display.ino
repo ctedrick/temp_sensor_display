6c103b33357f19f1bab134999d29da8b526738b9
Cody Tedrick <cody.tedrick@gmail.com>
2023-05-27 21:46:30 -0700

    Pick text

--- temp_sensor_display.ino
+++ temp_sensor_display.ino
@@ -7,7 +7,7 @@
 #include <DallasTemperature.h>
 #include <math.h>
 
-#pragma region 
+#pragma region
 
 // Data wire is plugged into port 2 on the Arduino
 #define ONE_WIRE_BUS 2
@@ -15,44 +15,82 @@
 // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
 OneWire oneWire(ONE_WIRE_BUS);
 
-// Pass our oneWire reference to Dallas Temperature. 
-DallasTemperature sensors(&oneWire);
+// Pass our oneWire reference to Dallas Temperature.
+DallasTemperature tempSensor(&oneWire);
 
-U8G2_SH1106_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+U8G2_SH1106_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0, /* reset=*/U8X8_PIN_NONE);
 
 #pragma endregion
 
-const char DEGREE_SYMBOL[] = {"\xb0"};
+DeviceAddress thermometerAddress;
+const char DEGREE_SYMBOL[] = { "\xb0" };
+
+const
+
+  float maxTemp = 0;
 
 void setup(void) {
-  sensors.begin();
+  tempSensor.begin();
+
   u8g2.begin();
+
+  if (!tempSensor.getAddress(thermometerAddress, 0)) {
+    Serial.println("Device not found!");
+  } else {
+    Serial.println("Device 0 Address: ");
+    printAddress(thermometerAddress);
+  }
+
+  tempSensor.setResolution(thermometerAddress);
 }
 
 void loop(void) {
-  sensors.requestTemperatures(); // Send the command to get temperatures
+  tempSensor.requestTemperatures();  // Send the command to get temperatures
 
-  float currTemp = sensors.getTempFByIndex(0);
+  float currTemp = tempSensor.getTempFByIndex(0);
 
-  u8g2.setFont(u8g2_font_ncenB14_tf);
   u8g2.firstPage();
   do {
-    u8g2.setCursor(0, 20);
+
+    u8g2.setFont(u8g2_font_tenthinguys_tr);
+    u8g2.setCursor(0, 9);
+    u8g2.setDrawColor(2);
+
+    u8g2.print("--Engine Temp--");
+    // u8g2.print(currTemp);
+
+    // u8g2.setFont(u8g2_font_t0_22_mn );
+    u8g2.setCursor(0, 42.6);
     u8g2.setDrawColor(2);
 
     u8g2.print(currTemp);
     u8g2.print(DEGREE_SYMBOL);
     u8g2.print("F");
-  } while ( u8g2.nextPage() );
+
+    // u8g2.setCursor(0, 64);
+    // u8g2.setDrawColor(2);
+
+    // u8g2.print(currTemp);
+    // u8g2.print(DEGREE_SYMBOL);
+    // u8g2.print("F");
+  } while (u8g2.nextPage());
 
   delay(1000);
 }
 
 void lcdBold(bool aVal) {
   if (aVal) {
-    u8g2.setFont(u8x8_font_saikyosansbold8_u);// BOLD
+    u8g2.setFont(u8x8_font_saikyosansbold8_u);  // BOLD
   } else {
-    u8g2.setFont(u8g2_font_ncenB14_tr);// NORMAL
+    u8g2.setFont(u8g2_font_ncenB14_tr);  // NORMAL
   }
 }
 
+// Serial print device address from the address array - for debugging
+
+void printAddress(DeviceAddress deviceAddress) {
+  for (uint8_t i = 0; i < 8; i++) {
+    if (deviceAddress[i] < 16) Serial.print("0");
+    Serial.print(deviceAddress[i], HEX);
+  }
+}
